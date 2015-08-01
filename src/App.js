7003e9133d0f7921ba3435944c723f66a3d9bf2f
Mr Speaker <mrspeaker@gmail.com>
2015-08-01 16:40:42 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import jsonp from 'jsonp';
-import qs from 'querystring';
+import querystring from 'querystring';
 import Rx from 'rx';
-import RxFuncSubject from './RxFuncSubject';
+import RxFunc from './lib/RxFuncSubject';
+import rand from './lib/rand';
 
-import constants from './constants';
-import player from './youtubePlayer';
 import Nav from './Nav';
 import InfoBox from './InfoBox';
+import Player from './Player';
 
-import rand from './rand';
+// TODO: Randomizes the huge global array of vid ids. Fix this, eh.
 const randVids = rand(window.allVids);
 
 class App extends React.Component {
@@ -17,30 +16,27 @@ class App extends React.Component {
   constructor (props) {
     super(props);
 
-    const queryString = qs.decode(window.location.search);
-    const vidId = queryString['v'] || queryString['?v'] || randVids[randVids.length - 1];
+    const qs = querystring.decode(window.location.search);
+    const vidId = qs['v'] || qs['?v'] || randVids[randVids.length - 1];
 
     this.state = {
-      player: player(640, 390, null, this.stateChange.bind(this)),
       title: '-',
       description: '-',
       vidId
     };
 
     this.handlers = {
-      next: RxFuncSubject(),
-      prev: RxFuncSubject(),
-      end: RxFuncSubject()
+      next: RxFunc(),
+      prev: RxFunc(),
+      end: RxFunc()
     };
+
+    this.stream = this.createVidIDStream(vidId);
+    this.onInfoLoad = this.onInfoLoad.bind(this);
   }
 
   componentDidMount () {
-    this.createVidIDStream(this.state.vidId).subscribe(v => {
-      this.loadVid(v);
-      this.loadVidInfo(v);
-      this.setState({vidId: v});
-      window.history.pushState({v}, v, `?v=${v}`);
-    });
+    this.stream.subscribe(v => this.onVideoChanged(v));
   }
 
   createVidIDStream (initVidId) {
@@ -52,55 +48,41 @@ class App extends React.Component {
       next.map(fwd),
       prev.map(back),
       end.map(fwd),
-      Rx.Observable.fromEvent(window, 'popstate').map(back)
-    )
-      .scan(0, (ac, e) => Math.max(0, ac + e))
-      .startWith(0)
-      .map(i => i == 0 ? initVidId : randVids[i - 1]);
+      Rx.Observable.fromEvent(window, 'popstate').map(back))
+        .scan(0, (ac, e) => Math.max(0, ac + e))
+        .startWith(0)
+        .map(i => i == 0 ? initVidId : randVids[i - 1]);
   }
 
-  stateChange (state) {
-    if (state.data === 0 /* ended */) {
-      this.handlers.end();
-    }
+  onVideoChanged (v) {
+    this.setState({
+      title: '...loading...',
+      description: '',
+      vidId: v
+    });
+
+    window.history.pushState({v}, v, `?v=${v}`);
   }
 
-  loadVid (vidId) {
-    const {player} = this.state;
-
-    player.then(p => p.loadVideoById({
-      videoId: vidId,
-      startSeconds: 3
-    }));
-  }
-
-  loadVidInfo (vidId) {
-    const base = 'https://www.googleapis.com/youtube/v3/videos';
-    const url = `${base}?id=${vidId}&key=${constants.APIKEY}&part=snippet&`;
-    jsonp(url, (err, data) => {
-      if (data && data.items) {
-        const {title, description} = data.items[0].snippet;
-        this.setState({
-          title,
-          description: description.replace(/\n\n/g, '\n')
-        });
-      }
+  onInfoLoad ({title, description}) {
+    this.setState({
+      title,
+      description: description.replace(/\n\n/g, '\n')
     });
   }
 
   render () {
     const {title, description, vidId} = this.state;
-    const {next, prev} = this.handlers;
-    const ytUrl = `https://www.youtube.com/watch?v=${vidId}`;
+    const {next, prev, end} = this.handlers;
 
     return (
       <div id="app">
         <div>
           <span className="title">{title}</span>&nbsp;
-          <a href={ytUrl}>{vidId}</a>
+          <a href={`https://www.youtube.com/watch?v=${vidId}`}>{vidId}</a>
           <Nav onNext={next} onPrev={prev}/>
         </div>
-        <div id="player"></div>
+        <Player vidID={vidId} onEnd={end} onInfoLoad={this.onInfoLoad} />
         <Nav onNext={next} onPrev={prev}/>
         <InfoBox content={description} />
       </div>
